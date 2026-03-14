22b00fad3c5db4a8a89ef148295c651f93a87ea3
jony-07 <javohir.habibjonov@webase.uz>
2026-03-14 13:36:09 +0500

    impoers fixed

--- packages/auth/env.d.ts
+++ packages/auth/env.d.ts
@@ -1,7 +1,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  // Добавляем все твои переменные сюда, чтобы TS их "узнал"
   readonly VITE_GOOGLE_CLIENT_ID: string;
+  readonly VITE_API_URL: string;
 }
 
 interface ImportMeta {
